f8319354bc8a19b7d0fab6e15248be6f218c7316
Janio Isacura <janioisacura@gmail.com>
2022-07-18 19:42:10 -0300

    NgRx package config

--- 02-redux-app/src/app/app.module.ts
+++ 02-redux-app/src/app/app.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { environment } from 'src/environments/environment';
 import { AppRoutingModule } from './app-routing.module';
 
-//NgRx Store
+//NgRx
 import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 //Custom reducer
 import { counterReducer } from './counter/counter.reducer';
 
@@ -12,6 +13,7 @@ import { AppComponent } from './app.component';
 import { ChildComponent } from './counter/child/child.component';
 import { GrandchildComponent } from './counter/grandchild/grandchild.component';
 import { NgrxExampleComponent } from './counter/ngrx-example/ngrx-example.component';
+
 
 @NgModule({
   declarations: [
@@ -23,7 +25,13 @@ import { NgrxExampleComponent } from './counter/ngrx-example/ngrx-example.compon
   imports: [
     BrowserModule,
     AppRoutingModule,
-    StoreModule.forRoot({ counter: counterReducer })
+    StoreModule.forRoot({ counter: counterReducer }),
+    StoreDevtoolsModule.instrument({
+      maxAge: 25, // Retains last 25 states
+      logOnly: environment.production, // Restrict extension to log-only mode
+      autoPause: true, // Pauses recording actions and state changes when the extension window is not open
+    }),
+
   ],
   providers: [],
   bootstrap: [AppComponent]
