17dac8dc811a4afea4f0ae384f0d0db459672c50
Janio Isacura <janioisacura@gmail.com>
2022-07-29 12:45:02 -0300

    Working on register form validations

--- 04-dashboard-app/src/app/auth/register/register.component.ts
+++ 04-dashboard-app/src/app/auth/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -8,9 +9,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  registerForm!: FormGroup;
+
+  constructor(private fb: FormBuilder) { }
+
+  get validName() { return this.registerForm.get('name')?.valid; }
+  get validEmail() { return this.registerForm.get('email')?.valid; }
+  get validPassword() { return this.registerForm.get('password')?.valid; }
+
+  get touchedName() { return this.registerForm.get('name')?.touched || this.registerForm.get('name')?.dirty; }
+  get touchedEmail() { return this.registerForm.get('email')?.touched || this.registerForm.get('email')?.dirty; }
+  get touchedPassword() { return this.registerForm.get('password')?.touched || this.registerForm.get('password')?.dirty; }
 
   ngOnInit(): void {
+    this.registerForm = this.fb.group({
+      name: [ '', Validators.required ],
+      email: [ '',  [Validators.required, Validators.email] ],
+      password: [ '', Validators.required ]
+    });
+  }
+
+  createUser(): void {
+    console.log(this.registerForm.valid);
+    console.log(this.registerForm.value);
   }
 
 }
