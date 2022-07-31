d686f2c2add2090608a1bb71370ac93c7dfecf0a
Janio Isacura <janioisacura@gmail.com>
2022-07-31 15:59:27 -0300

    register logic added

--- 04-dashboard-app/src/app/auth/register/register.component.ts
+++ 04-dashboard-app/src/app/auth/register/register.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-register',
@@ -11,7 +13,9 @@ export class RegisterComponent implements OnInit {
 
   registerForm!: FormGroup;
 
-  constructor(private fb: FormBuilder) { }
+  constructor(private fb: FormBuilder,
+              private authService: AuthService,
+              private router: Router) { }
 
   get validName() { return this.registerForm.get('name')?.valid; }
   get validEmail() { return this.registerForm.get('email')?.valid; }
@@ -25,13 +29,23 @@ export class RegisterComponent implements OnInit {
     this.registerForm = this.fb.group({
       name: [ '', Validators.required ],
       email: [ '',  [Validators.required, Validators.email] ],
-      password: [ '', Validators.required ]
+      password: [ '', [Validators.required, Validators.minLength(6)] ]
     });
   }
 
   createUser(): void {
-    console.log(this.registerForm.valid);
-    console.log(this.registerForm.value);
+    if (this.registerForm.invalid) return;
+
+    const { name, email, password } = this.registerForm.value;
+
+    this.authService.createUser(name, email, password)
+      .then(credentials => {
+        console.log(credentials);
+        this.router.navigate(['/']);
+      })
+      .catch(err => {
+        console.log(err);
+      })
   }
 
   inputValidationStyle(fieldName: string): string {
