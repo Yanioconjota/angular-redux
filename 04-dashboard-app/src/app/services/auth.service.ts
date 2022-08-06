8d04de58a6a3fa563dfab8eb52de839a95953427
Janio Isacura <janioisacura@gmail.com>
2022-08-06 13:59:25 -0300

    new users are now added to firestore db

--- 04-dashboard-app/src/app/services/auth.service.ts
+++ 04-dashboard-app/src/app/services/auth.service.ts
@@ -1,28 +1,47 @@
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
+import { User } from '../models/user.model';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor(public auth: AngularFireAuth) { }
+  constructor(public auth: AngularFireAuth,
+              private firestore: AngularFirestore) { }
 
   //Listens to all auth changes
   initAuthListener() {
     //receibes a firebase user
     this.auth.authState.subscribe((user)=>{
       if (user) {
-        const { displayName, uid, email } = user;
-        console.log(displayName, uid, email);
+        const { uid, email } = user;
+        console.log(uid, email);
       }
     })
   }
 
   createUser( name: string, email: string, password: string ) {
+    //returns a promis with a firebase user
     return this.auth.createUserWithEmailAndPassword(email, password)
+      .then(({user}) => {
+        const newUser = new User(
+          user?.uid,
+          name,
+          user?.email
+          );
+        //creates a document in firestore in the specified path
+        return this.firestore.doc(`${user?.uid}/user`)
+          //sets the data from newUser to that path
+          //Since firebase doesn't allow an instance of a class we use the spread operator to pass a new object.
+
+          /*Function DocumentReference.set() called with invalid data. Data must be an object, but it was: a custom User object (found in document bOV7WwKvV3f3vmjzvo64ODT44IG3/user) */
+          .set({...newUser})
+      })
   }
 
   login(email: string, password: string) {
