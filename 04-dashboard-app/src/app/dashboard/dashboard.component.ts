6164ec062fe59add0010b36fb8076240e6fcc1cb
Janio Isacura <janioisacura@gmail.com>
2022-08-19 15:27:47 -0300

    incomeExpenses listener method created

--- 04-dashboard-app/src/app/dashboard/dashboard.component.ts
+++ 04-dashboard-app/src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { AppState } from '../app.reducer';
+import { filter } from 'rxjs/operators';
+import { IncomeExpensesService } from '../services/income-expenses.service';
+import { User } from '../models/user.model';
+import * as incomeExpensesActions from '../income-expenses/income-expenses.actions';
 
 @Component({
   selector: 'app-dashboard',
@@ -6,11 +13,31 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class DashboardComponent implements OnInit {
+export class DashboardComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  subscriber!: Subscription
+
+  constructor(private store: Store<AppState>,
+              private incomeExpensesService: IncomeExpensesService) { }
 
   ngOnInit(): void {
+    this.subscriber = this.store.select('auth')
+      .pipe(
+        //we use the pipe operator because the user first appears as null and then we get the data
+        filter(auth => auth.user !== null)
+      )
+      .subscribe(({user}) => {
+        this.incomeExpensesService.initIncomeExpensesListener(user?.uid)
+          .subscribe( incomeExpensesFB => {
+            console.log(incomeExpensesFB);
+            this.store.dispatch(incomeExpensesActions.setItems({ items: incomeExpensesFB }))
+          })
+      });
+  }
+
+  ngOnDestroy(): void {
+    //unsubscribe on logout
+    this.subscriber.unsubscribe();
   }
 
 }
--- 04-dashboard-app/src/app/services/income-expenses.service.ts
+++ 04-dashboard-app/src/app/services/income-expenses.service.ts
@@ -1,6 +1,7 @@
+import { IncomeExpenses } from './../models/income-expenses.model';
 import { Injectable } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
-import { IncomeExpenses } from '../models/income-expenses.model';
+import { map } from 'rxjs/operators';
 import { AuthService } from './auth.service';
 
 @Injectable({
@@ -15,12 +16,30 @@ export class IncomeExpensesService {
 
     const uid = this.authService.user.uid;
 
+    console.log(uid);
+
     //this way we create a new firebase collection by passing its name as a string 'items' and pass the incomeExpenses as a new object
     return this.firestore.doc(`${ uid }/income-expenses`)
         .collection('items')
         .add({...incomeExpenses})
-        .then( ref => console.log(`${ref} added successfully`))
+        .then( ref => console.log(`${ref.id} added successfully`))
         .catch(console.warn);
 
+  }
+
+  initIncomeExpensesListener(uid: string | undefined ) {
+    return this.firestore.collection(`${ uid }/income-expenses/items`)
+      //Create a stream of synchronized changes. This method keeps the local array in sorted query order. --> we use it to gather firebase id from item
+      .snapshotChanges()
+      .pipe(
+        //we return a mapped snapshot with the firebase doc id
+        //The first map is an rxjs operator, the seond one is an array method to return the data in the way we desire
+        map(snapshot => snapshot.map( doc => ({
+              uid: doc.payload.doc.id,
+              ...doc.payload.doc.data() as IncomeExpenses
+            })
+          )
+        )
+      );
   }
 }
