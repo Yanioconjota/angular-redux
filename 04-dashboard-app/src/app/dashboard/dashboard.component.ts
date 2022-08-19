fb7d3c923638163a7074e47660281be33dda37a4
Janio Isacura <janioisacura@gmail.com>
2022-08-19 15:30:19 -0300

    unsubscription to incomeExpenses added

--- 04-dashboard-app/src/app/dashboard/dashboard.component.ts
+++ 04-dashboard-app/src/app/dashboard/dashboard.component.ts
@@ -4,7 +4,6 @@ import { Store } from '@ngrx/store';
 import { AppState } from '../app.reducer';
 import { filter } from 'rxjs/operators';
 import { IncomeExpensesService } from '../services/income-expenses.service';
-import { User } from '../models/user.model';
 import * as incomeExpensesActions from '../income-expenses/income-expenses.actions';
 
 @Component({
@@ -15,19 +14,20 @@ import * as incomeExpensesActions from '../income-expenses/income-expenses.actio
 })
 export class DashboardComponent implements OnInit, OnDestroy {
 
-  subscriber!: Subscription
+  userSubscriber!: Subscription;
+  incomeExpensesSubscriber!: Subscription
 
   constructor(private store: Store<AppState>,
               private incomeExpensesService: IncomeExpensesService) { }
 
   ngOnInit(): void {
-    this.subscriber = this.store.select('auth')
+    this.userSubscriber = this.store.select('auth')
       .pipe(
         //we use the pipe operator because the user first appears as null and then we get the data
         filter(auth => auth.user !== null)
       )
       .subscribe(({user}) => {
-        this.incomeExpensesService.initIncomeExpensesListener(user?.uid)
+        this.incomeExpensesSubscriber = this.incomeExpensesService.initIncomeExpensesListener(user?.uid)
           .subscribe( incomeExpensesFB => {
             console.log(incomeExpensesFB);
             this.store.dispatch(incomeExpensesActions.setItems({ items: incomeExpensesFB }))
@@ -37,7 +37,8 @@ export class DashboardComponent implements OnInit, OnDestroy {
 
   ngOnDestroy(): void {
     //unsubscribe on logout
-    this.subscriber.unsubscribe();
+    this.userSubscriber.unsubscribe();
+    this.incomeExpensesSubscriber.unsubscribe();
   }
 
 }
