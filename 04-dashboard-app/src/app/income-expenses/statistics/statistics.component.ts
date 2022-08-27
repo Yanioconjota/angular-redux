4f2b7230661e97dee1d806e55bcb0c7b855d883e
Janio Isacura <janioisacura@gmail.com>
2022-08-27 20:18:18 -0300

    statistics component populated with income expense data from the store

--- 04-dashboard-app/src/app/income-expenses/statistics/statistics.component.ts
+++ 04-dashboard-app/src/app/income-expenses/statistics/statistics.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { AppState } from 'src/app/app.reducer';
+import { IncomeExpenses } from '../../models/income-expenses.model';
 
 @Component({
   selector: 'app-statistics',
@@ -6,11 +10,38 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class StatisticsComponent implements OnInit {
+export class StatisticsComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  income = 0;
+  expenses = 0;
+  totalIncome = 0;
+  totalExpenses = 0;
+  subscriber!: Subscription;
+
+  constructor(private store: Store<AppState>) { }
 
   ngOnInit(): void {
+
+    this.subscriber = this.store.select('incomeExpenses').subscribe(({items}) => this.generateStatistics(items));
+  }
+
+  ngOnDestroy(): void {
+    this.subscriber.unsubscribe();
+  }
+
+  generateStatistics(items: IncomeExpenses[]) {
+    console.log(items);
+
+    for (const item of items) {
+      if (item.type === 'income') {
+        this.totalIncome += item.amount;
+        this.income ++;
+      } else {
+        this.totalExpenses += item.amount;
+        this.expenses ++;
+      }
+
+    }
   }
 
 }
