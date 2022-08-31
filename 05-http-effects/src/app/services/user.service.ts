5ff11dfe3d9777794f9ee71fb38af13a98ff3f19
Janio Isacura <janioisacura@gmail.com>
2022-08-31 16:21:30 -0300

    Http Effects Users app structure created

---
+++ 05-http-effects/src/app/services/user.service.ts
@@ -0,0 +1,24 @@
+import { map } from 'rxjs/operators';
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  private url = 'https://reqres.in';
+
+  constructor(private http: HttpClient) { }
+
+  getUsers() {
+    return this.http.get(`${this.url}/api/users?per_page=6`)
+      .pipe(
+        map((resp: any) => resp['data'])
+      );
+  }
+
+  getUserById(id: string) {
+  return this.http.get(`${this.url}/api/users/${id}`)
+  }
+}
