6a66e3a7525c5ba50ce4b5e47dde59d9f8c659ce
Janio Isacura <janioisacura@gmail.com>
2022-08-31 22:28:05 -0300

    Working on getUsers from NgRx

--- 05-http-effects/src/app/users/list/list.component.ts
+++ 05-http-effects/src/app/users/list/list.component.ts
@@ -1,6 +1,8 @@
 import { UserService } from './../../services/user.service';
 import { Component, OnInit } from '@angular/core';
 import { User } from 'src/app/models/user.model';
+import { Store } from '@ngrx/store';
+import { AppState } from 'src/app/store/app.reducer';
 
 @Component({
   selector: 'app-list',
@@ -11,13 +13,13 @@ export class ListComponent implements OnInit {
 
   users: User[] = [];
 
-  constructor(private userService: UserService) { }
+  constructor(private store: Store<AppState>) { }
 
   ngOnInit(): void {
-    this.userService.getUsers().subscribe( users => {
-      console.log(users);
-      this.users = users;
-    });
+    // this.userService.getUsers().subscribe( users => {
+    //   console.log(users);
+    //   this.users = users;
+    // });
   }
 
 }
