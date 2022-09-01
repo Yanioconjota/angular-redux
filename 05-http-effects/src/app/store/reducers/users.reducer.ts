822849d523cfb0bcc308d501e6b2b06ccee7f09f
Janio Isacura <janioisacura@gmail.com>
2022-08-31 22:05:49 -0300

    users reducer added

---
+++ 05-http-effects/src/app/store/reducers/users.reducer.ts
@@ -0,0 +1,37 @@
+import { createReducer, on } from '@ngrx/store';
+import { loadUsers, loadUsersSucess, loadUsersError } from '../actions';
+import { User } from '../../models/user.model';
+
+export interface UsersState {
+    users: User[],
+    loaded: boolean,
+    loading: boolean,
+    error: any
+}
+
+export const usersInitialState: UsersState = {
+  users: [],
+  loaded: false,
+  loading: false,
+  error: null
+}
+
+export const counterReducer = createReducer(usersInitialState,
+
+    on(loadUsers, state => ({ ...state, loading: true })),
+
+    on(loadUsersSucess, (state, { users }) => ({
+      ...state,
+      loading: false,
+      loaded: true,
+      users: [ ...users ]
+    })),
+
+    on(loadUsersError, (state, { payload }) => ({
+      ...state,
+      loading: false,
+      loaded: false,
+      error: payload
+    })),
+
+);
