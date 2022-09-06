1dcc5158c7f361dabc80d0ddd23c815b3c128467
Janio Isacura <janioisacura@gmail.com>
2022-09-05 21:30:19 -0300

    delay on response added

--- 05-http-effects/src/app/services/user.service.ts
+++ 05-http-effects/src/app/services/user.service.ts
@@ -12,7 +12,7 @@ export class UserService {
   constructor(private http: HttpClient) { }
 
   getUsers() {
-    return this.http.get(`${this.url}/api/users?per_page=6`)
+    return this.http.get(`${this.url}/api/users?per_page=6&delay=1`)
       .pipe(
         map((resp: any) => resp.data)
       );
